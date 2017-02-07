d74f24ae0b36b02485188354366e1200d9be2f0a
winnipass <adams4lyf@gmail.com>
2017-02-07 14:28:59 +0000

    Reports updated

--- resources/views/reports/surveys.blade.php
+++ resources/views/reports/surveys.blade.php
@@ -7,44 +7,56 @@
                             </tr><tr class='odd'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Excellent</td>
-                                                <td>2017-02-07 06:28:38</td>
+                                                <td>2017-02-07 14:17:34</td>
                                             </tr><tr class='even'><td>Spintex</td>
+                                                <td>How would you rate the service ?</td>
+                                                <td>Good</td>
+                                                <td>2017-02-07 14:17:27</td>
+                                            </tr><tr class='odd'><td>Spintex</td>
+                                                <td>How would you rate the service ?</td>
+                                                <td>Very Good</td>
+                                                <td>2017-02-07 14:17:17</td>
+                                            </tr><tr class='even'><td>Spintex</td>
+                                                <td>How would you rate the service ?</td>
+                                                <td>Excellent</td>
+                                                <td>2017-02-07 06:28:38</td>
+                                            </tr><tr class='odd'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Very Good</td>
                                                 <td>2017-02-07 06:27:48</td>
-                                            </tr><tr class='odd'><td>Spintex</td>
+                                            </tr><tr class='even'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Bad</td>
                                                 <td>2017-02-07 06:27:40</td>
-                                            </tr><tr class='even'><td>Spintex</td>
+                                            </tr><tr class='odd'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Good</td>
                                                 <td>2017-02-07 06:27:30</td>
-                                            </tr><tr class='odd'><td>Spintex</td>
+                                            </tr><tr class='even'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Good</td>
                                                 <td>2017-02-07 06:26:29</td>
-                                            </tr><tr class='even'><td>Spintex</td>
+                                            </tr><tr class='odd'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Very Good</td>
                                                 <td>2017-02-07 06:28:31</td>
-                                            </tr><tr class='odd'><td>Spintex</td>
+                                            </tr><tr class='even'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Excellent</td>
                                                 <td>2017-02-07 06:28:23</td>
-                                            </tr><tr class='even'><td>Spintex</td>
+                                            </tr><tr class='odd'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Good</td>
                                                 <td>2017-02-07 06:28:17</td>
-                                            </tr><tr class='odd'><td>Spintex</td>
+                                            </tr><tr class='even'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Excellent</td>
                                                 <td>2017-02-07 06:28:08</td>
-                                            </tr><tr class='even'><td>Spintex</td>
+                                            </tr><tr class='odd'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Very Good</td>
                                                 <td>2017-02-07 06:28:02</td>
-                                            </tr><tr class='odd'><td>Spintex</td>
+                                            </tr><tr class='even'><td>Spintex</td>
                                                 <td>How would you rate the service ?</td>
                                                 <td>Poor</td>
                                                 <td>2017-02-07 06:27:55</td>
